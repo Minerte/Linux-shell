7771709b0f92e28175ecd041d4c862e7570fdd8f
Minerte <169073521+Minerte@users.noreply.github.com>
2024-10-27 21:05:59 +0100

    Update installer.sh

--- Gentoo/installer/installer.sh
+++ Gentoo/installer/installer.sh
@@ -62,10 +62,8 @@ function setup_partitions() {
     btrfs subvolume create /mnt/root/home
 
     mkdir -p /mnt/gentoo
-    cd /mnt/gentoo ||  exit
-    mkdir /home || exit
-    mkdir /efi || exit
-    cd || exit
+    mkdir /mnt/gentoo/home || exit
+    mkdir /mnt/gentoo/efi || exit
 
     # /mnt/gentoo coming from wiki where root is suppose to be mounted
     mount -t btrfs -o defaults,noatime,compress=lzo,subvol=activeroot /dev/mapper/$crypt_name /mnt/gentoo
