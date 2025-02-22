08342bf8bb4d2ac679bb1f92f63565e8af7dd952
Minerte <169073521+Minerte@users.noreply.github.com>
2025-02-22 12:49:18 +0100

    Update scratch-encryption.sh

--- Gentoo/installer/scratch-encryption.sh
+++ Gentoo/installer/scratch-encryption.sh
@@ -38,6 +38,7 @@ function setup_disk() {
     fi
     
 # Live disk
+    echo "Editing disk for drive"
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk "$sel_disk"
 g # New GPT disklabel
 n # New partition 
@@ -57,6 +58,7 @@ q # exit
 EOF
 
 #Boot disk
+    echo "Editing disk for boot and key"
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk "$sel_disk_boot"
 g # New GPT disklabel
 n # new pratition
@@ -77,52 +79,72 @@ w # write to disk
 q # exit
 EOF
     # Start Prep for boot disk
+    echo "Making filesystem for boot and key"
     mkfs.vfat -F 32 "${sel_disk_boot}1"
     mkfs.ext4 "${sel_disk_boot}2"
+    echo "Making directory in /media/ to mount the key"
     mkdir /media/ex-usb
     mount "${sel_disk_boot}2" /media/ex-usb
     cd /media/ex-usb || { echo "Failed to change directory"; exit 1;}
     # End of prep for boot disk
 
     # Start prep for swap partition
+    echo "Generating random keyfile"
     dd if=/dev/urandom of=swap-keyfile bs=8388608 count=1 || { echo "Could not generate key for swap-keyfile"; exit 1; }
+    echo "GPG symmetric encryption for keyfile"
     gpg --symmetric --cipher-algo AES256 --output swap-keyfile.gpg swap-keyfile || { echo "Could not encrypt key with gpg --symmetric key for swap-keyfile"; exit 1; }
 
+    echo "Decrypting GPG keyfile"
     gpg --decrypt --output /tmp/swap-keyfile swap-keyfile.gpg || { echo "Could not decrypt key with gpg --symmetric key for swap-keyfile"; exit 1; }
+    echo "Encrypting swap partition with keyfile"
     cryptsetup luksFormat --type luks2 --cipher aes-xts-plain64 --key-size 512 --hash sha512 "${sel_disk}1" --key-file=/tmp/swap-keyfile || { echo "Could not encrypt swap partition with key-file swap-keyfile"; exit 1; }
 
+    echo "Opening up the swap partition"
     cryptsetup open "${sel_disk}1" cryptswap --key-file=/tmp/swap-keyfile || { echo "Could not open the encrypted swap partition"; exit 1; }
+    echo "Shreding keyfile"
     shred -u /tmp/swap-keyfile
     
     # Swap partition setup
+    echo "Making swap and swapon"
     mkswap /dev/mapper/cryptswap || { echo "Failed to make swap"; exit 1; }
     swapon /dev/mapper/cryptswap || { echo "No swap on"; exit 1; }
     # End of prep for swap partition
 
     # Start prep for root partition
+    echo "Generating random keyfile"
     dd if=/dev/urandom of=luks-keyfile bs=8388608 count=1 || { echo "Could not generate key for luks-keyfile"; exit 1; }
+    echo "GPG symmetric encryption for keyfile"
     gpg --symmetric --cipher-algo AES256 --output luks-keyfile.gpg luks-keyfile || { echo "Could not encrypt key with gpg --symmetric key for luks-keyfile"; exit 1; }
 
+    echo "Decrypting GPG keyfile"
     gpg --decrypt --output /tmp/luks-keyfile luks-keyfile.gpg || { echo "Could not decrypt key with gpg --symmetric key for luks-keyfile"; exit 1; }
+    echo "Encrypting root partition with keyfile"
     cryptsetup luksFormat --type luks2 --cipher aes-xts-plain64 --key-size 512 --hash sha512 "${sel_disk}2" --key-file=/tmp/luks-keyfile || { echo "Could encrypt root partition with key-file luks-keyfile"; exit 1; }
 
+    echo "Opening up the root partition"
     cryptsetup open "${sel_disk}2" cryptroot --key-file=/tmp/luks-keyfile || { echo "Could not open the encrypted root partition"; exit 1; }
+    echo "Shreding keyfile"
     shred -u /tmp/luks-keyfile
     # End of prep for root partition
 
     cd || { echo "Failed to change to user root directory"; exit 1; }
 
     # Root partition setup
+    echo "Making /mnt/root for mount of subvolumes"
     mkdir /mnt/root || { echo "Failed to create directory"; exit 1; }
+    echo "Makeing btrfs filesystem"
     mkfs.btrfs -L BTROOT /dev/mapper/cryptroot || { echo "Failed to create btrfs"; exit 1; }
+    echo "mounting filesystem to /mnt/root"
     mount -t btrfs -o defaults,noatime,compress=zstd /dev/mapper/cryptroot /mnt/root || { echo "Failed to mount btrfs /dev/mapper/cryptroot to /mnt/root"; exit 1; }
 
     # Create subvolumes
+    echo "creation of subvolumes"
     for sub in activeroot home etc var log tmp; do
         btrfs subvolume create "/mnt/root/$sub" || { echo "Failed to create subvolume $sub"; exit 1; }
     done
 
     # Creating and mounting to root
+    echo "Mounting everything to /mnt/gentoo"
     mount -t btrfs -o defaults,noatime,compress=zstd,subvol=activeroot /dev/mapper/cryptroot /mnt/gentoo/
     mkdir /mnt/gentoo/{home,etc,var,log,tmp}
     for sub in home etc var log tmp; do
@@ -152,10 +174,29 @@ function Download_stage3file() {
     
     cd || { echo "failed to change directory to root"; exit 1 ; }
     echo "Please don't exit the link menu berfor the download is completed"
-    echo "select you choosen stage 3 file"
+    echo "select you choosen stage3 file"
+    echo "and also if you want to verify download the tar.xz.asc stage3 file"
     sleep 10
     links https://www.gentoo.org/downloads/mirrors/
     sleep 10
+
+    echo -e "\nDo you want to verify the stage3 file? (y/n): "
+    read -r user_input
+
+    if [[ "$user_input" =~ ^[Yy] ]]; then
+        cd || { echo "failed to change directory to root"; exit 1 ; }
+        gpg --import /usr/share/openpgp-keys/gentoo-release.asc
+        sleep 5
+        gpg --verify stage3-*.tar.xz.asc stage3-*.tar.xz
+
+        if [[ $? -ne 0 ]]; then
+        echo "Exiting..."
+        exit 1
+        fi
+    else
+        echo "Skipping verifictation"
+    fi
+
     cd || { echo "failed to change directory to root"; exit 1 ; }
     echo "Extracting stage3 file..."
     tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner -C /mnt/gentoo || { echo "Failed to extract"; exit 1; }
@@ -178,14 +219,18 @@ LABEL=BTROOT    /tmp    btrfs   defaults,noatime,nosuid,nodev,noexec,compress=zs
 EOF
 
     sleep 5
+    echo "setting up loclale.gen"
     sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /mnt/gentoo/etc/locale.gen
     # If dualboot uncomment below
     # sed -i "s/clock=\"UTC\"/clock=\"local\"/g" ./etc/conf.d/hwclock
+    echo "Changing to swedish keyboard"
     sed -i "s/keymap=\"us\"/keymaps=\"sv-latin1\"/g" /mnt/gentoo/etc/conf.d/keymaps
+    echo "change locale.conf and edit timezone to Europe/Stockholm"
     echo 'LANG="en_US.UTF-8"' >> /mnt/gentoo/etc/locale.conf
     echo 'LC_COLLATE="C.UTF-8"' >> /mnt/gentoo/etc/locale.conf
     echo "Europe/Stockholm" > /mnt/gentoo/etc/timezone
 
+    echo "Succesfully edited basic system"
 }
 
 function config_portage () {
@@ -193,9 +238,11 @@ function config_portage () {
     mkdir -p /mnt/gentoo/etc/portage/repos.conf
     cp /usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
     cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+    echo "we will now have network in chroot later"
     sleep 5
 
     # Copy custom portage configuration files
+    echo "Moving over portge file from download to chroot"
     mkdir /mnt/gentoo/etc/portage/env
     mv ~/Linux-shell-main/Gentoo/portage/env/no-lto /mnt/gentoo/etc/portage/env/
     mv ~/Linux-shell-main/Gentoo/portage/make.conf /mnt/gentoo/etc/portage/
@@ -235,7 +282,6 @@ read -r -p "Enter the disk you want to partition and format for Boot (e.g., /dev
 read -r -p "Enter the disk you want to partition and format for Root/swap(e.g., /dev/sda): " selected_disk
 validate_block_device "$selected_disk" "$selected_disk_Boot"
 setup_disk "$selected_disk" "$selected_disk_Boot"
-move_encryption
 Download_stage3file
 config_system
 config_portage
